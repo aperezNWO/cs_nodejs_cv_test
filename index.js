4e53ec8207a9266efe1c177b9b05d7cedfcf24f2
aperezNWO <alejandro.perez.acosta@hotmail.com>
2024-12-18 20:26:39 -0500

    image processing.

--- index.js
+++ index.js
@@ -1,41 +1,18 @@
 //const Jimp = require('jimp');
-const sharp = require('sharp');
+const sharp       = require('sharp');
+const { JSDOM }   = require('jsdom');
+const dom         = new JSDOM('<html><body></body></html>');
+const window      = dom.window;
+const document    = window.document;
 
 const outputImagePath = '.img/output.jpg';  // Output image
 
 // Define a global variable 'Module' with a method 'onRuntimeInitialized':
   async function onRuntimeInitialized() {
-
-    
-
+    //
     try {
-/*
-      // load local image file with jimp. It supports jpg, png, bmp, tiff and gif:
-      var jimpSrc = await Jimp.read('./img/square.jpg');
-  
-      // `jimpImage.bitmap` property has the decoded ImageData that we can use to create a cv:Mat
-      var src = cv.matFromImageData(jimpSrc.bitmap);
-    
-      // following lines is copy&paste of opencv.js dilate tutorial:
-      let dst = new cv.Mat();
-      let M = cv.Mat.ones(5, 5, cv.CV_8U);
-      let anchor = new cv.Point(-1, -1);
-      cv.dilate(src, dst, M, anchor, 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
-    
-      // Now that we are finish, we want to write `dst` to file `output.png`. For this we create a `Jimp`
-      // image which accepts the image data as a [`Buffer`](https://nodejs.org/docs/latest-v10.x/api/buffer.html).
-      // `write('output.png')` will write it to disk and Jimp infers the output format from given file name:
-      new Jimp({
-        width: dst.cols,
-        height: dst.rows,
-        data: Buffer.from(dst.data)
-      })
-      .write('./img/output.png');
-    
-      src.delete();
-      dst.delete();
-      */
       sharp('./img/square.jpg')
+      /*
       .resize(256, 256)
       .toFile('./img/output.jpg', (err, info) => {
         if (err) {
@@ -43,37 +20,14 @@ const outputImagePath = '.img/output.jpg';  // Output image
         } else {
           console.log('Image processed:', info);
         }
-      })
+      })*/
       .toBuffer()
       .then((data) => {
-        /*  
-        const mat = cv.imread(data);  // Convert the image to a matrix
-
-        const gray = new cv.Mat();
-        cv.cvtColor(mat, gray, cv.COLOR_RGBA2GRAY, 0); // Convert image to grayscale
-
-        const faces = new cv.RectVector();
-        const classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_ALT2);  // Use OpenCV's built-in face detector
-
-        // Detect faces in the image
-        classifier.detectMultiScale(gray, faces, 1.1, 3, 0);
-
-        // Draw rectangles around faces
-        for (let i = 0; i < faces.size(); i++) {
-          const face = faces.get(i);
-          cv.rectangle(mat, face, [255, 0, 0, 255], 2);
-        }
-
-        cv.imshow('outputCanvas', mat);  // Display the processed image in a canvas element
-        mat.delete();  // Clean up
-        gray.delete();
-        faces.delete();
-        classifier.delete();
-    
-        console.log('Image processed and saved at', outputImagePath);
+        //
+        console.log('Image processing...');
       })
       .catch((err) => {
-        console.error('Error processing the image:', err);*/
+        console.error('Error processing the image:');
       });
   
     } catch (error) {
